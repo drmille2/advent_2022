5a9e8c4e1dc93a5d10827f4651483395a0b8ddc6
David Miller <davmille@starbucks.com>
2022-12-03 07:17:32 -0800

    add priority scoring

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -15,3 +15,21 @@ struct Cli {
 fn main() {
     println!("Hello, world!");
 }
+
+fn to_priority(c: &char) -> Option<i32> {
+    let offset: i32 = if c.is_lowercase() { -9 } else { 17 };
+    c.to_digit(36).and_then(|x| Some(x as i32 + offset))
+}
+
+#[cfg(test)]
+mod test {
+    use crate::to_priority;
+
+    #[test]
+    fn test_to_priority() {
+        assert_eq!(to_priority(&'a').unwrap(), 1);
+        assert_eq!(to_priority(&'z').unwrap(), 26);
+        assert_eq!(to_priority(&'A').unwrap(), 27);
+        assert_eq!(to_priority(&'Z').unwrap(), 52);
+    }
+}
