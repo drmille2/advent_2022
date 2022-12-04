29f60990fc81d3afa0902ee5a5d858794b27a62c
David Miller <davmille@starbucks.com>
2022-12-03 19:34:42 -0800

    day 3 part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashMap, fs};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 use clap::Parser;
 
@@ -18,7 +21,8 @@ fn main() {
     let cli_args = Cli::parse();
 
     let input = &fs::read_to_string(cli_args.input).unwrap();
-    println!("{}", solve_part1(input));
+    println!("Part 1: {}", solve_part1(input));
+    println!("Part 2: {}", solve_part2(input));
 }
 
 fn to_priority(item: &char) -> Option<i32> {
@@ -46,6 +50,39 @@ fn return_duplicates(compartments: (&str, &str)) -> Vec<char> {
     out
 }
 
+/// panics if items is not evenly divisble by group_len
+fn group_items(items: Vec<&str>, group_len: usize) -> Vec<Vec<&str>> {
+    let mut out = Vec::new();
+
+    let mut g = 0;
+    let groups = items.len() / group_len;
+    while g < groups {
+        out.push(vec![]);
+        g += 1;
+    }
+
+    for item in items.into_iter().enumerate() {
+        let group_num = item.0 / group_len;
+        out[group_num].push(item.1);
+    }
+
+    out
+}
+
+fn find_common_elements(group: Vec<&str>) -> Vec<char> {
+    let mut sets: Vec<HashSet<char>> = Vec::new();
+    for g in group {
+        let mut set = HashSet::new();
+        g.chars().map(|x| set.insert(x)).for_each(drop);
+        sets.push(set);
+    }
+    sets.into_iter()
+        .reduce(|a, i| i.intersection(&a).map(|x| *x).collect::<HashSet<char>>())
+        .unwrap()
+        .into_iter()
+        .collect::<Vec<char>>()
+}
+
 fn solve_part1(input: &str) -> i32 {
     input
         .split_terminator("\n")
@@ -55,9 +92,19 @@ fn solve_part1(input: &str) -> i32 {
         .sum()
 }
 
+fn solve_part2(input: &str) -> i32 {
+    group_items(input.split_terminator("\n").collect(), 3)
+        .into_iter()
+        .map(find_common_elements)
+        .map(|x| x.into_iter().map(|y| to_priority(&y).unwrap()).sum::<i32>())
+        .sum()
+}
+
 #[cfg(test)]
 mod test {
-    use crate::{return_duplicates, sep_compartments, to_priority};
+    use crate::{
+        find_common_elements, group_items, return_duplicates, sep_compartments, to_priority,
+    };
 
     #[test]
     fn test_to_priority() {
@@ -89,6 +136,34 @@ mod test {
         assert_eq!(
             return_duplicates(("jqHRNqRjqzjGDLGL", "rsFMfFZSrLrFZsSL")),
             vec!['L']
+        );
+    }
+
+    #[test]
+    fn test_group_items() {
+        assert_eq!(
+            group_items(vec!["a", "b", "c", "d", "e", "f"], 3),
+            vec![vec!["a", "b", "c"], vec!["d", "e", "f"]]
+        );
+    }
+
+    #[test]
+    fn test_find_common_elements() {
+        assert_eq!(
+            find_common_elements(vec![
+                "vJrwpWtwJgWrhcsFMMfFFhFp",
+                "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                "PmmdzqPrVvPwwTWBwg"
+            ]),
+            vec!['r']
+        );
+        assert_eq!(
+            find_common_elements(vec![
+                "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                "ttgJtRGJQctTZtZT",
+                "CrZsJsPPZsGzwwsLwLmpwMDw"
+            ]),
+            vec!['Z']
         );
     }
 }
