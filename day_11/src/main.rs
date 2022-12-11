2a308b690680f827c19b7334470a0f58c7ef1d03
David Miller <davmille@starbucks.com>
2022-12-11 13:59:07 -0800

    day 11 part 1

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -13,21 +13,97 @@ struct Cli {
     input: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Monkey {
-    items: VecDeque<usize>,
+    items: VecDeque<u128>,
     op: Op,
-    test_div: usize,
+    test_div: u128,
     if_true: usize,
     if_false: usize,
+    ins_count: usize,
 }
 
-impl Monkey {}
+impl Monkey {
+    fn inspect(&mut self) -> Option<u128> {
+        if let Some(item) = self.items.pop_front() {
+            let item = match self.op {
+                Op::Add(val) => item + val,
+                Op::Mult(val) => dbg!(item * val),
+                Op::Square => item * item,
+            };
+            self.ins_count += 1;
+            Some(item / 3)
+        } else {
+            None
+        }
+    }
+    fn throw(&mut self) -> Option<(usize, u128)> {
+        if let Some(item) = self.inspect() {
+            if item % self.test_div == 0 {
+                Some((self.if_true, item))
+            } else {
+                Some((self.if_false, item))
+            }
+        } else {
+            None
+        }
+    }
+
+    fn catch(&mut self, item: u128) {
+        self.items.push_back(item);
+    }
+}
 
 #[derive(Debug)]
+struct Barrel {
+    monkeys: Vec<Monkey>,
+}
+
+impl Barrel {
+    fn from_str(s: &str) -> Self {
+        let mut monkeys = Vec::new();
+        let rows = s.split_terminator("\n").collect();
+        for monkey_str in group_items(rows, 7) {
+            let items = parse_starting_items(monkey_str[1]);
+            let op = parse_op(monkey_str[2]);
+            let test_div = parse_test_div(monkey_str[3]);
+            let (if_true, if_false) = parse_throw((&monkey_str[4], &monkey_str[5]));
+            monkeys.push(Monkey {
+                items,
+                op,
+                test_div,
+                if_true,
+                if_false,
+                ins_count: 0,
+            })
+        }
+        Barrel { monkeys }
+    }
+    fn throw_and_catch(&mut self, pos: usize) -> Option<bool> {
+        if let Some(throw) = self.monkeys[pos].throw() {
+            self.monkeys[throw.0].catch(throw.1);
+            Some(true)
+        } else {
+            None
+        }
+    }
+    fn do_round(&mut self) {
+        for m in 0..self.monkeys.len() {
+            loop {
+                if let Some(_) = self.throw_and_catch(m) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 enum Op {
-    Add(usize),
-    Mult(usize),
+    Add(u128),
+    Mult(u128),
     Square,
 }
 
@@ -50,7 +126,7 @@ fn group_items(items: Vec<&str>, group_len: usize) -> Vec<Vec<&str>> {
     out
 }
 
-fn parse_starting_items(s: &str) -> VecDeque<usize> {
+fn parse_starting_items(s: &str) -> VecDeque<u128> {
     let mut out = VecDeque::new();
     let items_str = s.split_once(":").unwrap().1;
     for item in items_str.split(",") {
@@ -80,48 +156,42 @@ fn parse_op(s: &str) -> Op {
     }
 }
 
-fn parse_last_num(s: &str) -> usize {
-    s.split(" ").reduce(|a, i| i).unwrap().parse().unwrap()
+fn parse_last_num(s: &str) -> u128 {
+    s.split(" ").reduce(|_, i| i).unwrap().parse().unwrap()
 }
 
-fn parse_test_div(s: &str) -> usize {
+fn parse_test_div(s: &str) -> u128 {
     parse_last_num(s)
 }
 
 fn parse_throw(s: (&str, &str)) -> (usize, usize) {
-    (parse_last_num(s.0), parse_last_num(s.1))
+    (parse_last_num(s.0) as usize, parse_last_num(s.1) as usize)
 }
 
-fn parse_input(s: &str) -> Vec<Monkey> {
-    let mut monkeys = Vec::new();
-    // let rows = s.split("\n").collect();
-    let rows = s.split_terminator("\n").collect();
-    for monkey_str in group_items(rows, 7) {
-        let items = parse_starting_items(monkey_str[1]);
-        let op = parse_op(monkey_str[2]);
-        let test_div = parse_test_div(monkey_str[3]);
-        let (if_true, if_false) = parse_throw((&monkey_str[4], &monkey_str[5]));
-        monkeys.push(Monkey {
-            items,
-            op,
-            test_div,
-            if_true,
-            if_false,
-        })
+fn solve_part1(s: &str) -> usize {
+    let mut barrel = Barrel::from_str(s);
+    for _ in 0..20 {
+        barrel.do_round();
     }
-    monkeys
-}
-fn solve_part1(s: &str) {
-    let monkeys = parse_input(s);
-    dbg!(monkeys);
+    let mut sorted_monkeys = barrel.monkeys.clone();
+    sorted_monkeys.sort_by(|a, b| b.ins_count.cmp(&a.ins_count));
+    sorted_monkeys[0].ins_count * sorted_monkeys[1].ins_count
 }
 
-fn solve_part2(s: &str) {}
+fn solve_part2(s: &str) -> usize {
+    let mut barrel = Barrel::from_str(s);
+    for r in 0..7000 {
+        dbg!(r);
+        barrel.do_round();
+    }
+    let mut sorted_monkeys = barrel.monkeys.clone();
+    sorted_monkeys.sort_by(|a, b| b.ins_count.cmp(&a.ins_count));
+    sorted_monkeys[0].ins_count * sorted_monkeys[1].ins_count
+}
 
 fn main() {
     let cli_args = Cli::parse();
     let input = &fs::read_to_string(cli_args.input).unwrap();
-    println!("Part 1: \n");
-    solve_part1(input);
-    // println!("Part 2: {}", solve_part2(input));
+    println!("Part 1: {}", solve_part1(input));
+    println!("Part 2: {}", solve_part2(input));
 }
